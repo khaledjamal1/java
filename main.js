e668cee4ee6b1e7acb0a72db03f2dcff0eb26f2b
khaledjamal1 <khalidfaqari@gmail.com>
2022-04-06 01:31:33 +0300

    first try

---
+++ main.js
@@ -0,0 +1,10 @@
+function round(playerselect,compselect){
+
+}
+function playerselect(){
+    let playerselect = prompt('please enter your choice', " ");
+    return playerselect ;
+}
+function compselect(){
+    return compselect = random(['rock','paper','scissor']);
+}
