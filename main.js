dafcec042ee8a4b942df29aa838fe81c68944166
khaledjamal1 <khalidfaqari@gmail.com>
2022-04-06 03:09:48 +0300

    getting somewhere

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
-function round(playerselect,compselect){
 
+function playerSelection(){
+    let input = prompt("enter choice");
+    while (input==null){
+        input = prompt("enter choice");
+    }
+    return input;
 }
-function playerselect(){
-    let playerselect = prompt('please enter your choice', " ");
-    return playerselect ;
+function computerSelection(){
+    let options = ["rock","paper","scissor"]
+    return (options[Math.floor(Math.random()*options.length)]);
 }
-function compselect(){
-    return compselect = random(['rock','paper','scissor']);
+const playerSelection1 = playerSelection()
+const computerSelection1 = computerSelection()
+function playRound(playerSelection,computerSelection){
+     if (playerSelection1 =="rock" && computerSelection1 == 'paper'){
+       console.log ('you lose');
+    } else if (playerSelection1=="paper" && computerSelection1 == 'rock'){
+        console.log('you win');
+    } else if (playerSelection1=="scissor" && computerSelection1 == 'paper'){
+        console.log('you win');
+    } else if (playerSelection1=="paper" && computerSelection1 == 'scissor'){
+        console.log('you lose');
+    } else if (playerSelection1=="scissor" && computerSelection1 == 'rock'){
+        console.log('you lose');
+    } else if (playerSelection1=="rock" && computerSelection1 == 'scissor'){
+        console.log('you win');
+    } else if (playerSelection1=="scissor" && computerSelection1 == 'paper'){
+        console.log('you win');
+    } else if (playerSelection1=="paper" && computerSelection1 == 'scissor'){
+        console.log('you lose');
+    } else {
+        console.log('ss')
+    }
 }
+playRound()
